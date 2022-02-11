2b98887a1bc26a36951e331119e71ea7d539c657
David Alexander <dalexander@gmail.com>
2022-02-11 00:43:26 +0000

    Created Models

---
+++ Models/Spice.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace spicerack.Models
+{
+    public class Spice
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Manufacturer { get; set; }
+        public decimal? Price { get; set; }
+        public decimal? OuncesTotal { get; set; }
+        public decimal? OuncesRemaining { get; set; }
+        public int? Rating { get; set; }
+        public List<string>? Tags { get; set; }
+
+        public virtual ICollection<Rub> Rubs { get; set; }
+    }
+}
